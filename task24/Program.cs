f10bc36c9eb1d076d2c9071dcd6ec873ee43e81e
Sergio <sergio977@rambler.ru>
2022-11-25 15:18:47 +0400

    add task24

---
+++ task24/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36
+*/
+int SumOfRange(int start, int finish)
+{
+    int sum = 0;
+    for (int i = start; i <= finish; i++)
+    {
+        sum += i; // sum = sum + i;
+    }
+    return sum;
+}
+int getNumberFromUser(string message)
+{
+    int result = 0;
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int userNumber = getNumberFromUser("Введите А для подсчета суммы 1...А");
+int sum = SumOfRange(1, userNumber);
+Console.WriteLine($"Сумма чисел от 1...{userNumber} = {sum}");
